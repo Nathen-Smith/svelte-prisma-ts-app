e1de3b3359d4d3a68202be876fe540e08bb11206
Nathen Smith <nathencsmith@gmail.com>
2021-07-02 14:32:10 -0700

    saved web scraper code

--- server/src/HTMLParser.ts
+++ server/src/HTMLParser.ts
@@ -1,6 +1,16 @@
 export const HTMLParser = (html: string) => {
-  if (html.search(/<script type="application\/ld\+json"/) !== -1) {
-    return "yes!";
+  const metadataStartIdx = html.search(/<script type="application\/ld\+json">/);
+  if (metadataStartIdx !== -1) {
+    const metadataEndIdx =
+      html.slice(metadataStartIdx).search(/<\/script>/) + metadataStartIdx + 9;
+    const metadata = html.slice(metadataStartIdx, metadataEndIdx);
+
+    // This file is a template used to parse HTML from web scraping
+    // console.log(JSON.stringify(metadata));
+    // console.log(`start is ${metadataStartIdx}`);
+    // console.log(`end is ${metadataEndIdx}`);
+    // console.log(html.slice(metadataStartIdx, metadataEndIdx));
+    // https://www.google.com/search?q=biceps+workout
   }
   return "no";
 };
